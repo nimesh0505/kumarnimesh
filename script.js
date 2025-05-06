92dd5d43ac61eef6bd8b554f2027bb0e07988a57
Nimesh Kumar <nimesh.kumar031@gmail.com>
2025-05-06 16:19:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const body = document.body
 
 const btnTheme = document.querySelector('.fa-moon')
-const btnHamburger = document.querySelector('.fa-bars')
 
 const addThemeClass = (bodyClass, btnClass) => {
   body.classList.add(bodyClass)
@@ -16,7 +15,6 @@ addThemeClass(getBodyTheme, getBtnTheme)
 const isDark = () => body.classList.contains('dark')
 
 const setTheme = (bodyClass, btnClass) => {
-
 	body.classList.remove(localStorage.getItem('portfolio-theme'))
 	btnTheme.classList.remove(localStorage.getItem('portfolio-btn-theme'))
 
@@ -30,22 +28,6 @@ const toggleTheme = () =>
 	isDark() ? setTheme('light', 'fa-moon') : setTheme('dark', 'fa-sun')
 
 btnTheme.addEventListener('click', toggleTheme)
-
-const displayList = () => {
-	const navUl = document.querySelector('.nav__list')
-
-	if (btnHamburger.classList.contains('fa-bars')) {
-		btnHamburger.classList.remove('fa-bars')
-		btnHamburger.classList.add('fa-times')
-		navUl.classList.add('display-nav-list')
-	} else {
-		btnHamburger.classList.remove('fa-times')
-		btnHamburger.classList.add('fa-bars')
-		navUl.classList.remove('display-nav-list')
-	}
-}
-
-btnHamburger.addEventListener('click', displayList)
 
 const scrollUp = () => {
 	const btnScrollTop = document.querySelector('.scroll-top')
